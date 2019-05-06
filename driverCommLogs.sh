02e085ace136af70a8c45463c907e201c7fa16d1
ubaid <ubaid_hafeez@hotmail.com>
2019-05-06 02:02:20 -0400

    added scripts for communication logging experiments

---
+++ driverCommLogs.sh
@@ -0,0 +1,12 @@
+python commLogsToyMM.py 2 commLogs/
+python commLogsToyMM.py 3 commLogs/
+python commLogsToyMM.py 4 commLogs/
+# python commLogsToyMM.py 5 commLogs/
+# python commLogsToyMM.py 6 commLogs/
+# python commLogsToyMM.py 7 commLogs/
+# python commLogsToyMM.py 8 commLogs/
+# python commLogsToyMM.py 9 commLogs/
+# python commLogsToyMM.py 10 commLogs/
+# python commLogsToyMM.py 11 commLogs/
+# python commLogsToyMM.py 12 commLogs/
+# python commLogsToyMM.py 13 commLogs/
