6ab4f5da51b6ec64ac2b590a776e4ceabf5e53f4
ubaid <ubaid_hafeez@hotmail.com>
2020-02-04 09:59:10 -0500

    added new files to compare gpu performance

---
+++ newExp/c2gMulti.py
@@ -0,0 +1,82 @@
+import tensorflow as tf
+import json
+import sys
+from tensorflow.python.client import timeline
+from protobuf_to_dict import protobuf_to_dict
+from google.protobuf.json_format import MessageToJson
+from tensorflow.core.protobuf import rewriter_config_pb2
+from tensorflow.python.framework import graph_io
+import os
+import time
+
+
+
+# dim = 32*(2**9)
+dev1 = '/gpu:0'
+dev2 = '/gpu:1'
+dev3 = '/cpu:0'
+logPath = sys.argv[1]
+
+
+with tf.device(dev1):
+    X, Z1, _X = [], [], []
+    for i in range(9):
+        dim = 32*(2**i)
+        X.append(tf.random_uniform([dim, dim], 0, 10, name='X' + str(i)))
+        _X.append(tf.placeholder(dtype=tf.float32, shape=[dim, dim]))
+        Z1.append(tf.matmul(_X[i], _X[i]))
+
+with tf.device(dev2):
+    Y, Z2, _Y = [], [], []
+    for i in range(9):
+        dim = 32*(2**i)
+        Y.append(tf.random_uniform([dim, dim], 0, 10, name='Y' + str(i)))
+        _Y.append(tf.placeholder(dtype=tf.float32, shape=[dim, dim]))
+        Z2.append(tf.matmul(_Y[i], _Y[i]))
+
+with tf.device(dev3):
+    Z3 = []
+    for i in range(9):
+        Z3.append(tf.add(Z2[i], Z1[i]))
+
+
+config_proto = tf.ConfigProto(graph_options=tf.GraphOptions(build_cost_model=1))
+config_proto.intra_op_parallelism_threads = 1
+config_proto.inter_op_parallelism_threads = 1
+config_proto.graph_options.optimizer_options.opt_level = -1
+config_proto.graph_options.rewrite_options.constant_folding = (rewriter_config_pb2.RewriterConfig.OFF)
+config_proto.graph_options.rewrite_options.arithmetic_optimization = (rewriter_config_pb2.RewriterConfig.OFF)
+config_proto.graph_options.rewrite_options.dependency_optimization = (rewriter_config_pb2.RewriterConfig.OFF)
+config_proto.graph_options.rewrite_options.layout_optimizer = (rewriter_config_pb2.RewriterConfig.OFF)
+
+
+sess = tf.Session(config=config_proto)
+sess.run(tf.global_variables_initializer())
+
+X_, Y_ = sess.run([X, Y])
+X_Y_ = X_ + Y_
+_X_Y = _X + _Y
+
+tot_time = 0
+for i in range(10):
+    print(i)
+    run_metadata = tf.RunMetadata()
+    run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE, output_partition_graphs=True)
+    st = time.time()
+    sess.run(Z1+Z2+Z3,
+                {_i: i_ for _i, i_ in zip(_X_Y, X_Y_)},
+                options=run_options,
+                run_metadata=run_metadata)
+    tot_time += time.time() -st
+
+    if i >= 2:
+        jsonObj = MessageToJson(run_metadata)
+        with open('%s/metadata_matmul_%d.json' % (logPath, i), 'w') as outfile:
+            json.dump(jsonObj, outfile)
+
+        trace = timeline.Timeline(step_stats=run_metadata.step_stats)
+        trace_file = open('%s/timeline_matmul_%d.ctf.json' % (logPath, i), 'w')
+        trace_file.write(trace.generate_chrome_trace_format())
+print('total time taken : ', tot_time)
+
+
