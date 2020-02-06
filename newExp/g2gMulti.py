86fa9750cfe9f8cdfbbf526571e1c650b71c6220
ubaid <ubaid_hafeez@hotmail.com>
2020-02-06 12:39:51 -0500

    fixed bug which was creating same sized matrices

--- newExp/g2gMulti.py
+++ newExp/g2gMulti.py
@@ -24,7 +24,7 @@ with tf.device(dev1):
         dim = 32*(2**i)
         X.append(tf.random_uniform([dim, dim], 0, 10, name='X' + str(i)))
         _X.append(tf.placeholder(dtype=tf.float32, shape=[dim, dim]))
-        Z1.append(tf.matmul(_X[0], _X[0]))
+        Z1.append(tf.matmul(_X[i], _X[i]))
 
 with tf.device(dev2):
     Z3 = []
