527b1eeaa2252b2240ee9fb9efec8af5e720a451
xilenteyex <ubaid_hafeez@hotmail.com>
2020-03-05 23:07:23 +0000

    added new logs for comms

--- getComms/selectedComms.py
+++ getComms/selectedComms.py
@@ -11,19 +11,18 @@ import time
 
 
 
-# dim = 32
+dim = 8
 dev1 = '/gpu:0'
 dev2 = '/gpu:1'
 dev3 = '/cpu:0'
 logPath = sys.argv[1]
 dim = int(sys.argv[2])
 
-
 with tf.device(dev1):
     X, Z1, _X, Z2, Z3 = [], [], [], [], []
     X.append(tf.random_uniform([dim, dim], 0, 10, name='X' + str(0)))
     _X.append(tf.placeholder(dtype=tf.float32, shape=[dim, dim]))
-    Z1.append(tf.matmul(_X[0], _X[0]))
+    Z1.append(tf.matmul(_X[i], _X[i]))
 
 config_proto = tf.ConfigProto(graph_options=tf.GraphOptions(build_cost_model=1))
 config_proto.intra_op_parallelism_threads = 1
@@ -38,9 +37,7 @@ config_proto.graph_options.rewrite_options.layout_optimizer = (rewriter_config_p
 sess = tf.Session(config=config_proto)
 sess.run(tf.global_variables_initializer())
 
-X_, Y_ = sess.run([X, Y])
-X_Y_ = X_ + Y_
-_X_Y = _X + _Y
+X_ = sess.run(X)
 
 tot_time = 0
 for i in range(10):
@@ -49,18 +46,18 @@ for i in range(10):
     run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE, output_partition_graphs=True)
     st = time.time()
     sess.run(Z1+Z2+Z3,
-                {_i: i_ for _i, i_ in zip(_X_Y, X_Y_)},
+                {_i: i_ for _i, i_ in zip(_X, X_)},
                 options=run_options,
                 run_metadata=run_metadata)
     tot_time += time.time() -st
 
     if i >= 2:
         jsonObj = MessageToJson(run_metadata)
-        with open('%s/metadata_matmul_%d.json' % (logPath, i), 'w') as outfile:
+        with open('%s/metadata_%d.json' % (logPath, i), 'w') as outfile:
             json.dump(jsonObj, outfile)
 
         trace = timeline.Timeline(step_stats=run_metadata.step_stats)
-        trace_file = open('%s/timeline_matmul_%d.ctf.json' % (logPath, i), 'w')
+        trace_file = open('%s/timeline_%d.ctf.json' % (logPath, i), 'w')
         trace_file.write(trace.generate_chrome_trace_format())
 print('total time taken : ', tot_time)
 
